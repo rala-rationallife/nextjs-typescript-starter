71d8e3374da93e59b4dea400358a8d304c0b157e
rala-rationallife <s.tsuduki26@gmail.com>
2023-04-08 22:14:50 +0900

    Reset CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { Layout } from "@/components/layout"
-import "@/styles/globals.css"
+import "@/styles/globals.scss"
 import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
