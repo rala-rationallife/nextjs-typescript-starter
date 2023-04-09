c80f5ee059ee973dd7d8ba2ec5a430bb4b10d306
rala-rationallife <s.tsuduki26@gmail.com>
2023-04-09 15:02:26 +0900

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Meta } from "@/components/meta"
+
+export default function Custom404() {
+  return (
+    <>
+      <Meta pageTitle="404 - ページが見つかりません" />
+
+      <h1>ページが見つかりません</h1>
+    </>
+  )
+}
